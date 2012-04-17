41bd69c169eb76f06309d8e548c764944a8668f9
Giang NGUYEN <ndgiang84@gmail.com>
2012-04-17 09:20:14 +0700

    Added links to about/contact. Added more info to Sign Up page. Renamed Issue to issue in migration because PG on Heroku is case-sensitive.

--- db/migrate/20120413031904_addfieldsto_issues.rb
+++ db/migrate/20120413031904_addfieldsto_issues.rb
@@ -1,8 +1,6 @@
 class AddfieldstoIssues < ActiveRecord::Migration
   def change
-      add_column :Issues, :status, :string
-      add_column :Issues, :feeling, :string
+      add_column :issues, :status, :string
+      add_column :issues, :feeling, :string
   end
-
-
 end
